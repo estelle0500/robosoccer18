83c861d63c072d6ddefd282a5b5bd7bc141fcabe
Estelle <estelle0500@gmail.com>
2018-01-31 17:45:37 +0800

    Create compass_tests.h

---
+++ tests/compass_tests.h
@@ -0,0 +1,8 @@
+#ifndef COMPASS_TESTS_H
+
+/*
+Purpose: Test if compass is working
+Expected Action: Value of angle will be printed to Serial
+Troubleshooting: Check wire connection with compass
+*/
+void testCompass();
