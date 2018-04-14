e4feba0ce2e83eded52ecac9aeb4d627350f6917
Estelle <estelle0500@gmail.com>
2018-04-14 08:53:39 +0800

    Change light sensor interrupts to polling

--- robosoccer/src/logic/update_sensors.h
+++ robosoccer/src/logic/update_sensors.h
@@ -4,6 +4,7 @@
 #include "Arduino.h"
 #include "../components/compass.h"
 #include "../components/ir_sensor.h"
+#include "../components/light_sensor.h"
 #include "../logic/movement.h"
 
 // #define VERBOSE
@@ -11,8 +12,12 @@
 // interval in microseconds
 #define INTERVAL 20000
 
+extern volatile int countdown;
+extern volatile int trig;
+
 extern Compass compass;
 extern IRSensor *ir_sensors[4];
+extern LightSensor *light_sensors[4];
 
 void updateDriving();
 void updateAll();
